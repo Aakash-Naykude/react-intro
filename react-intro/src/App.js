8f3efa2c26633387bf746ee20d5294c26f73afbb
Aakash-Naykude <aakashnaykude123@gmail.com>
2021-12-23 22:37:19 +0530

    done

--- react-intro/src/App.js
+++ react-intro/src/App.js
@@ -1,26 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import "./Style.css";
 function App() {
   return (
     <div className="App">
-      <h1 style={{color:"green", fontSize:"50px"}}>Windows Operating system</h1>
+      <h1 style={{ color: "green", fontSize: "50px" }}>
+        Windows Operating system
+      </h1>
       <Newapp />
-      <h1 style={{color:"blue", fontSize:"50px"}}>Mobile Manufacturer</h1>
+      <h1 style={{ color: "blue", fontSize: "50px" }}>Mobile Manufacturer</h1>
       <Newapp2 />
     </div>
   );
 }
-function Newapp(){
-  const arr = ["iphone", "blackberrry", "windows", "iphone"]
-  return <ul>
-    {arr.map((e)=>(<li>{e}</li>))}
-  </ul>
+function Newapp() {
+  const arr = ["iphone", "blackberrry", "windows", "iphone"];
+  return (
+    <ul>
+      {arr.map((e) => (
+        <li>{e}</li>
+      ))}
+    </ul>
+  );
 }
-function Newapp2(){
-  const arr = ["Samsung", "HTC", "Micromax", "Apple"]
-  return <ul>
-    {arr.map((e)=>(<li>{e}</li>))}
-  </ul>
+function Newapp2() {
+  const arr = ["Samsung", "HTC", "Micromax", "Apple"];
+  return (
+    <ul>
+      {arr.map((e) => (
+        <li>{e}</li>
+      ))}
+    </ul>
+  );
 }
 export default App;
